a8a179d8bb6c5974b9df52a30807b98617ef9946
debaycisse <debaycisse@gmail.com>
2022-08-03 16:58:40 +0100

    started record or real data creation

--- app.py
+++ app.py
@@ -106,28 +106,34 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
+  real_data = Venue(
+    name = "The Musical Hop"
+    city = "San Francisco"
+  )
+  db.session.add(real_data)
+
   data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
+  "city": "San Francisco",
+  "state": "CA",
+  "venues": [{
+    "id": 1,
+    "name": "The Musical Hop",
+    "num_upcoming_shows": 0,
   }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    "id": 3,
+    "name": "Park Square Live Music & Coffee",
+    "num_upcoming_shows": 1,
   }]
-  return render_template('pages/venues.html', areas=data);
+  }, {
+  "city": "New York",
+  "state": "NY",
+  "venues": [{
+    "id": 2,
+    "name": "The Dueling Pianos Bar",
+    "num_upcoming_shows": 0,
+  }]
+  }]
+  return render_template('pages/venues.html', areas=data)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
